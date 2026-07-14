5417061c496b073556b50ef0a11bb633c37a9431
risaev <goodw2369@gmail.com>
2026-07-15 01:35:45 +0300

    вынесение тестов в отдельные процедуры и добавление метода получение первого направления

--- main.cpp
+++ main.cpp
@@ -5,6 +5,12 @@
 using Vector = std::vector<double>;
 using Matrix = std::vector<Vector>;
 
+struct SVDComponent {
+    Vector u;
+    double sigma;
+    Vector v;
+};
+
 Matrix transpose(const Matrix& matrix){
 
     size_t rows = matrix.size();
@@ -86,10 +92,35 @@ void print_vec(const Vector& vec){
     std::cout << std::endl;
 }
 
-int main(){
+SVDComponent getFirstSVDComponent(const Matrix& A, int maxIterations = 100) {
+    
+    size_t rows = A.size();
+    size_t cols = A[0].size();
+
+    Matrix transpor_A = transpose(A);
+
+    Vector v(cols,1.0); // случайный вектор 
+    v = normalize(v);
+
+    for(int i = 0; i < maxIterations; i++){
+
+        Vector y = multiply(A,v); // поворачиваем матрицей А вектор v
+        Vector x = multiply(transpor_A,y); // это как раз A^T * A * y. перемножение матриц усиливает растсяжение вдоль главной оси 
+        v = normalize(x); // вектор х как то изменился в размерах, нам нужно ток направление 
+
+    }
+    
+    Vector u_raw = multiply(A,v);
+    double sigma = getNorm(u_raw);
+    Vector u = normalize(u_raw);
+
+    return {u,sigma,v};
+}
+
+void start_math_test(){
     Matrix test_matr = {{1,2},{3,4}};
     Vector test_vect = {3,4};
-    std::cout << "///// Тестирование //////" << std::endl;
+    std::cout << "///// Тестирование математики //////" << std::endl;
 
     std::cout << "-> Изначальный вектор и матрица:" << std::endl;
     std::cout << "-> Вектор: " << std::endl;
@@ -113,5 +144,42 @@ int main(){
     Vector normalized_vec = normalize(test_vect);
     print_vec(normalized_vec); 
 
+}
+
+void start_power_iteration_test(){
+    std::cout << "///// Тестирование степенного метода //////" << std::endl;
+    Matrix test_svd_matrix = {{3.0,2.0},{2.0,3.0}};
+
+    std::cout << " * Изначальная матрица:" << std::endl;
+    print_matr(test_svd_matrix);
+
+    SVDComponent comp = getFirstSVDComponent(test_svd_matrix,100);
+
+    std::cout << " * Результаты SVD для первого приближения :" << std::endl;
+    std::cout << " * Сигма (ожидаемые результаты около 5.0): " << comp.sigma << std::endl << std::endl;
+    
+    std::cout << " * Вектор U (ожидамые результаты ~0.707, ~0.707):" << std::endl;
+    print_vec(comp.u);
+    
+    std::cout << " * Вектор V (ожидаемые результаты ~0.707, ~0.707):" << std::endl;
+    print_vec(comp.v);
+
+    // начинаем пытаться восстановить матрицу
+    size_t rows = comp.u.size();
+    size_t cols = comp.v.size();
+    Matrix A_approx(rows, Vector(cols, 0.0));
+
+    for (size_t i = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j) {
+            A_approx[i][j] = comp.sigma * comp.u[i] * comp.v[j];
+        }
+    }
+
+    std::cout << "--- Восстановленная матрица (должна быть близка к оригиналу) ---" << std::endl;
+    print_matr(A_approx);
+}
+int main(){
+    start_math_test();
+    start_power_iteration_test
     return 0;
-} 
+}
